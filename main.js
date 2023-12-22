294b0667f97c53151be45535600a544ca52f63bf
Marco Santiago <barzha001@gmail.com>
2023-12-21 19:00:19 -0600

    Terminado

---
+++ main.js
@@ -0,0 +1,81 @@
+//Act 1
+
+/*let usuario = prompt ('Ingresa un numero');
+let numero1 = 5;
+console.log(usuario, 'numero de...')
+while (numero1<=1000) {
+    if (numero1<=usuario)
+    console.log(numero1, 'multiplo de 5')
+    numero1 = numero1+5;
+    
+}*/
+
+
+//Act 2
+
+/*let usuario1 = prompt('Ingresa un número')
+let usuario2 = prompt ('Ingresa otro número')
+let numero = 1;
+while (numero<=50 ) {
+    if(numero==usuario1)
+    {
+        console.log(numero, 'Loteria')
+    }
+    else if(numero==usuario2)
+    {
+        console.log(numero, 'Loteria');
+    }
+    else{
+        console.log(numero)
+    }
+    numero++;
+    
+}*/
+//Act 3
+/*let numeroslista = [];
+i = 0;
+let numeros;
+
+while (!(numeros=="")) {
+    numeros = prompt('Ingresa tus numeros del 1 al 10: ');
+    numeroslista.push(numeros);
+}
+console.log('Introduciste estos valores: ', numeroslista);*/
+//Act4
+/*
+i=0;
+while (i<5) {
+    let clientes = prompt('Ingrese letras o palabras')
+    i++
+    console.log(clientes)
+}*/
+//Act5
+/*i=0;
+while (i<7) {
+    let diadelasemana = prompt('Ingresa un dia de la semana')
+    if(diadelasemana == 'lunes'){
+        alert('Inicia con toda la energia tu semana')
+    }
+    else if(diadelasemana=='martes'){
+        alert('Que floja esta la semana')
+    }
+    else if(diadelasemana=='miercoles'){
+    alert('Ya es ombligo de semana')
+    }
+    else if(diadelasemana=='jueves'){
+    alert('Casi fin de semana')
+    }
+    else if(diadelasemana=='viernes'){
+    alert('Es viernes y el cuerpo lo sabe')
+    }    
+    else if(diadelasemana=='sabado'){
+    alert('Es fin de semana')
+    }
+    else if(diadelasemana == 'domingo'){
+    alert('Ve a descansar')
+    }
+    else{
+        alert('Ingresa un dia de la semana imbécil')
+    }
+    i++
+}*/
